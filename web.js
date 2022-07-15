5d99abdc8aba9f7c23a64e48a4b3bbb80aa4d2cf
therealkevind <47948540+therealkevind@users.noreply.github.com>
2022-07-15 11:40:35 -0400

    add basic ejs templating

--- web.js
+++ web.js
@@ -4,9 +4,16 @@ const app = express();
 
 app.use(express.static("public"));
 
+app.set("views", "./views");
+app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+  res.render("index", {
+    inviteUrl: `https://discord.com/api/oauth2/authorize?client_id=${process.env.clientId}&permissions=2147516416&scope=bot%20applications.commands`
+  });
+});
 
 const listener = app.listen(process.env.PORT, () => {
-  console.log(`Listening on port ${listener.address().port}`)
+  console.log(`Listening on port ${listener.address().port}`);
 });
 
 export { app as default };
