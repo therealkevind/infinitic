29e4e0348dc470e1f245154314b35bbdd4389451
therealkevind <47948540+therealkevind@users.noreply.github.com>
2022-07-14 23:41:59 -0400

    set up website

--- public/main.js
+++ public/main.js
@@ -2,17 +2,69 @@ import {X, O, Board, Game} from "./board.js";
 
 document.addEventListener("DOMContentLoaded", ()=>{
 
-  const game = Game.empty(Board.nested(3n));
+  let game;
+
+  // const game = Game.empty(Board.nested(3n));
 
   const gameCanvas = document.getElementById("game-board"),
+    gameForm = document.getElementById("game"),
     coordInput = document.getElementById("coords"),
     moveButton = document.getElementById("move");
 
-  gameCanvas.width = game.getWidth();
-  gameCanvas.height = game.getHeight();
-  game.draw(gameCanvas);
+  {
+    const widthInput = document.getElementById("width"),
+      heightInput = document.getElementById("height"),
+      depthInput = document.getElementById("depth"),
+      goalInput = document.getElementById("goal"),
+      settingsForm = document.getElementById("settings");
 
-  moveButton.addEventListener("click", ()=>{
+    const onSizeInput = () => {
+      goalInput.placeholder = Math.min(widthInput.value || 3, heightInput.value || 3);
+      goalInput.max = Math.max(widthInput.value || 3, heightInput.value || 3);
+    }
+
+    widthInput.addEventListener("input", onSizeInput);
+    heightInput.addEventListener("input", onSizeInput);
+    onSizeInput();
+
+    settingsForm.addEventListener("submit", e => {
+      e.preventDefault();
+
+      game = Game.empty(Board.nested(BigInt(depthInput.value || depthInput.placeholder), BigInt(widthInput.value || widthInput.placeholder), BigInt(heightInput.value || heightInput.placeholder), BigInt(goalInput.value || goalInput.placeholder)));
+      gameCanvas.width = game.getWidth();
+      gameCanvas.height = game.getHeight();
+      try {
+        game.draw(gameCanvas);
+      } catch {
+        alert("It seems this game is too big for your browser to render. Try a smaller game.");
+        gameCanvas.width = 0;
+        gameCanvas.height = 0;
+
+        let cause;
+        if (depthInput.value > 1) {
+          cause = depthInput;
+        } else if ((widthInput.value || widthInput.placeholder) ** 2 < heightInput.value) {
+          cause = heightInput;
+        } else {
+          cause = widthInput;
+        }
+        cause.focus();
+        cause.select();
+        return;
+      }
+
+      coordInput.disabled = false;
+      moveButton.disabled = false;
+      coordInput.placeholder = game.requirementsA1();
+      coordInput.value = "";
+
+      coordInput.focus();
+    });
+  }
+
+  gameForm.addEventListener("submit", e => {
+    e.preventDefault();
+
     if (!game.whoseTurn) return alert("Game's over.");
 
     const coordText = coordInput.value;
@@ -28,9 +80,19 @@ document.addEventListener("DOMContentLoaded", ()=>{
           case X: alert("X wins!"); break;
           case O: alert("O wins!"); break;
           default: alert("It's a tie.");
-        }
+        } else coordInput.placeholder = game.requirementsA1();
         game.draw(gameCanvas);
       }
     } else alert("Invalid coordinates!");
+
+    coordInput.value = "";
+    if (game.winner) {
+      coordInput.disabled = true;
+      moveButton.disabled = true;
+      document.getElementById("settings-detail").open = true;
+    } else {
+      coordInput.focus();
+      coordInput.select();
+    }
   });
 });
